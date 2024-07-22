7846f068e408cdaea3f254c7910ef3839d945536
Justin Steffen <jcsteffen03@gmail.com>
2024-07-21 19:21:06 -0600

    test passed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -5,29 +5,45 @@ class Coffee:
     def __init__(self, name):
         self.name = name
         Coffee.all.append(self)
-    
+
     def get_name(self):
         return self._name
-    
+
     def set_name(self, value):
-        if type(value) is str and len(value) >= 3 and not hasattr(self, "name"):
+        if type(value) is str and 3 <= len(value) and not hasattr(self, "name"):
             self._name = value
         else:
-            print("cannot change the name of the coffee")
+            print("NO")
 
-    name = property(get_name, set_name)
-
+    name = property(get_name, set_name)  
+        
     def orders(self):
-        pass
+        
+        my_orders = []
+
+        for order in Order.all:
+            if order.coffee == self:
+                my_orders.append(order)
+        return my_orders
     
     def customers(self):
-        pass
+        
+        my_Customers = []
+        
+        for order in Order.all:
+            if order.coffee == self and order.customer not in my_Customers:
+                my_Customers.append(order.customer)
+        return my_Customers
     
     def num_orders(self):
-        pass
+        my_orders = self.orders()
+        return len(my_orders)
     
     def average_price(self):
-        pass
+        my_orders = self.orders()
+        if len(my_orders) > 0:
+            return sum(order.price for order in my_orders) / len(my_orders)
+
 
 class Customer:
 
@@ -39,27 +55,36 @@ class Customer:
         
     def get_name(self):
         return self._name
-    
+
     def set_name(self, value):
         if type(value) is str and 1 <= len(value) <= 15:
             self._name = value
         else:
-            print("Not a name")
-        
-    name = property(get_name, set_name)
+            print("NO")
 
+    name = property(get_name, set_name)  
+    
     def orders(self):
+        
+        my_orders = []
+
         for order in Order.all:
-            if order.customer == self and type(order.customer) is Customer:
-                return order.customer
-            else:
-                print("no")
+            if order.customer == self:
+                my_orders.append(order)
+        return my_orders
     
     def coffees(self):
-        pass
+        
+        my_coffees = []
+        
+        for order in Order.all:
+            if order.customer == self and order.coffee not in my_coffees:
+                my_coffees.append(order.coffee)
+        return my_coffees
+        
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
     
 class Order:
 
@@ -78,6 +103,28 @@ class Order:
         if type(value) is float and 1.0 <= value <= 10.0 and not hasattr(self, "price"):
             self._price = value
         else:
-            print("cannot change the price of the coffee")
-
+            print("No")
+        
     price = property(get_price, set_price)
+
+    def get_customer(self):
+        return self._customer
+    
+    def set_customer(self, value):
+        if type(value) is Customer:
+            self._customer = value
+        else:
+            print("No!")
+    
+    customer = property(get_customer, set_customer)
+
+    def get_coffee(self):
+        return self._coffee
+    
+    def set_coffee(self, value):
+        if type(value) is Coffee:
+            self._coffee = value
+        else:
+            print("No!")
+    
+    coffee = property(get_coffee, set_coffee)
